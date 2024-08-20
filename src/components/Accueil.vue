f61fef68f9711905d3add00513584665dada163a
elaneg <elane.grd@gmail.com>
2024-08-20 16:18:03 +0200

    feat.accueil: page d'accueil modifiée et title aussi

---
+++ src/components/Accueil.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="accroche">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Accroche",
+    data() {
+        return {
+            message: "Bienvenue sur mon portfolio universitaire !",
+        };
+    },
+};
+</script>
+
+<style scoped>
+.accroche {
+    text-align: center;
+    margin-top: 50px;
+    font-size: 24px;
+    color: #333;
+}
+</style>
